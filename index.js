0ca6aeda25fcb9eda2f87c30c272d1e5d6b5c545
codewithkevin <oseikelvin709@gmail.com>
2022-06-03 01:05:22 +0000

    update

--- index.js
+++ index.js
@@ -1,62 +1,7 @@
+ReactDOM.render(<h1>Hello God</h1>, document.getElementById('root'));
 
-function Navbar() {
-    return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-                </ul>
-            </div>
-        </nav>
-    )
-}
-
-function Maincontent() {
-    return (
-       <h1>Welcome to my website</h1>
-    )
-}
-
-function Hello() {
-    return (
-        <div>
-            <p>
-                Hello, my name is <strong>Rajat</strong>
-            </p>
-        </div>
-    )
-}
-
-ReactDOM.render(
-    <div>
-        <Navbar/>
-        <Maincontent />
-        <Hello />
-    </div>,
-    document.getElementById('root')
-);
-
+const h1 = document.createElement('h1');
+h1.textContent = 'This is your boy kevin God.';
+h1.className = 'title';
+document.getElementById('root').appendChild(h1);
