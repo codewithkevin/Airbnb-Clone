aaf9d1b55609062f97bdb44e03e1225e57fa9929
codewithkevin <oseikelvin709@gmail.com>
2022-06-03 01:21:31 +0000

    update

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-ReactDOM.render(<h1>Hello God</h1>, document.getElementById('root'));
+ReactDOM.render(
+    
+     <New />,
+     document.getElementById('root'));
 
 
 const h1 = document.createElement('h1');
 h1.textContent = 'This is your boy kevin God.';
 h1.className = 'title';
 document.getElementById('root').appendChild(h1);
+
+
+
+
+function New(){
+    return (
+        <h4>My space</h4>
+    )
+}
