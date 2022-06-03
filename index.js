d8793c03f67f1351a2772184657aa32615fd09c2
codewithkevin <oseikelvin709@gmail.com>
2022-06-03 10:14:04 +0000

    update

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-ReactDOM.render(
-    
-     <New />,
-     document.getElementById('root'));
-
-
+//DOM
 const h1 = document.createElement('h1');
 h1.textContent = 'This is your boy kevin God.';
 h1.className = 'title';
-document.getElementById('root').appendChild(h1);
+document.getElementById('root').append(h1);
+
+//Element JSX 
+const element = 
+    <div className="header-section"> 
+        <h1>Hello World</h1>
+        <p>lorem Ips</p>
+    </div>
 
 
 function New(){
     return (
-        <h4>My space</h4>
+        <h4>My space Hello</h4>
     )
 }
+
+ReactDOM.render(
+    <div> 
+        <h1>Hello World</h1>
+        <p>lorem Ips</p>
+    </div>, 
+    document.getElementById('root')
+)
