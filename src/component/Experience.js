efa7d983c378f0df8bb195f6a3615d7b5d811123
codewithkevin <oseikelvin709@gmail.com>
2022-06-05 22:02:11 +0000

    update

---
+++ src/component/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Image from './images/image 12.png';
+
+
+const Experience = () => {
+    return (
+        <div className="card">
+            <img src={Image} className="card-imgage" alt="..." />
+        </div>
+    )
+}
