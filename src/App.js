a6ae2d69172142a18a87b7b1653f857772122039
codewithkevin <oseikelvin709@gmail.com>
2022-06-05 15:03:40 +0000

    update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import Navbar from './component/Navbar';
+
+const App = () => {
+  return (
+    <div className='App'>
+      
+      <div className='Navbar'>
+        <Navbar />
+      </div>
+
+     </div> 
+  )
+};
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h2 className="logo">Airbin</h2>
+        </nav>
+    )
+}
+
+
+export default Navbar;
