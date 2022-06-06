8997a277f4b9c86e83d0a7e1e1c3213951539632
codewithkevin <oseikelvin709@gmail.com>
2022-06-06 21:52:24 +0000

    cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Image from "./Images/image 12.png";
-import Star from "./Images/Star 1-1.png";
+
 
 
 import Navbar from './component/Navbar';
 import Herosection from './component/Hero';
 import Experience from './component/Experience';
-import Data from './component/Data';
+import data from './component/data';
 
 const App = () => {
 
-  const cards = Data.map(item => {
+  const cards = data.map(item => {
     return (
       <Experience
         img={item.coverImg}
         rating={item.stats.rating}
         reviewCount={item.stats.reviewCount}
+        description={item.description}
         price={item.price}
         location={item.location}
         openSpots={item.openSpots}
@@ -37,31 +37,9 @@ const App = () => {
       </div> 
 
       <div className='Experience'>
-        <Experience 
-          img={Image}
-          star={Star}
-          title='The Space'
-          country='United States'
-          rating='4.5'
-          prices='$100'
-          
-        />
-        <Experience 
-         img={Image}
-         star={Star}
-         title='The Space'
-         country='United States'
-         rating='4.5'
-         prices='$100'
-        />
-        <Experience 
-          img={Image}
-          star={Star}
-          title='The Space'
-          country='United States'
-          rating='4.5'
-          prices='$100'
-        />
+
+      {cards}
+      
         
       </div>    
 
--- src/component/data.js
+++ src/component/data.js
@@ -1,10 +1,13 @@
+import Image from "./Images/image 12.png";
+import Star from "./Images/Star 1-1.png";
+
 export default [
     {
         id: 1,
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: "katie-zaferes.png",
+        coverImg: Image,
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -17,7 +20,7 @@ export default [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: "wedding-photography.png",
+        coverImg: Image,
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -30,7 +33,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "mountain-bike.png",
+        coverImg: Image,
         stats: {
             rating: 4.8,
             reviewCount: 2
